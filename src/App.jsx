57dcaf27aa509288a9615b5b3ea93e476f2c1c8e
Alessandro Brugioni <alessandro.brugioni@gmail.com>
2023-07-31 16:29:02 +0200

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,62 +1,18 @@
-import { useState } from 'react';
-import Header from './components/Header';
-import Modal from './components/Modal';
+import { Routes, Route } from 'react-router-dom';
 
-import NewTask from './components/NewTask';
-import TaskControl from './components/TaskControl';
-import TaskList from './components/TaskList';
+import HomePage from './pages/Home';
+import AboutPage from './pages/About';
+import Header from './components/Header';
 
 function App() {
-  const [isAddingTask, setIsAddingTask] = useState(false);
-  const [tasks, setTasks] = useState([]);
-  const [appliedFilter, setAppliedFilter] = useState('all');
-
-  const displayedTasks = tasks.filter((task) => {
-    if (appliedFilter === 'all') {
-      return true;
-    }
-    return task.category === appliedFilter;
-  });
-
-  function startAddTaskHandler() {
-    setIsAddingTask(true);
-  }
-
-  function cancelAddTaskHandler() {
-    setIsAddingTask(false);
-  }
-
-  function addTaskHandler(taskData) {
-    setTasks((prevTasks) => {
-      return [
-        ...prevTasks,
-        {
-          id: Math.random().toString(),
-          ...taskData,
-        },
-      ];
-    });
-    setIsAddingTask(false);
-  }
-
-  function setFilterHandler(category) {
-    setAppliedFilter(category);
-  }
-
   return (
     <>
-      {isAddingTask && (
-        <Modal onClose={cancelAddTaskHandler}>
-          <NewTask onAddTask={addTaskHandler} onCancel={cancelAddTaskHandler} />
-        </Modal>
-      )}
       <Header />
       <main>
-        <TaskControl
-          onStartAddTask={startAddTaskHandler}
-          onSetFilter={setFilterHandler}
-        />
-        <TaskList tasks={displayedTasks} />
+        <Routes>
+          <Route path="" element={<HomePage />} />
+          <Route path="/about" element={<AboutPage />} />
+        </Routes>
       </main>
     </>
   );
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,23 @@
-import './Header.css';
-import logo from '../assets/logo.png';
+import { Link } from 'react-router-dom';
+
+import classes from './Header.module.css';
 
 function Header() {
-  return (
-    <header className="main-header">
-      <img src={logo} alt="A list" />
-      <h1>My Cypress Course Tasks</h1>
-    </header>
-  );
+    return (
+      <header className={classes.header}>
+        <h1>Cypress Demo</h1>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/" data-cy="header-home-link">Home</Link>
+            </li>
+            <li>
+              <Link to="/about" data-cy="header-about-link">About</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    );
 }
 
 export default Header;
