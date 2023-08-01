b3a5fb3d00f148ee8f4872109f38041478fb1a1c
Alessandro Brugioni <alessandro.brugioni@gmail.com>
2023-08-01 11:50:48 +0200

    first commit

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,5 +1,6 @@
+/// <reference types="cypress" />
 // ***********************************************
-// This example commands.js shows you how to
+// This example commands.ts shows you how to
 // create various custom commands and overwrite
 // existing commands.
 //
@@ -23,3 +24,38 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+//
+// declare global {
+//   namespace Cypress {
+//     interface Chainable {
+//       login(email: string, password: string): Chainable<void>
+//       drag(subject: string, options?: Partial<TypeOptions>): Chainable<Element>
+//       dismiss(subject: string, options?: Partial<TypeOptions>): Chainable<Element>
+//       visit(originalFn: CommandOriginalFn, url: string, options: Partial<VisitOptions>): Chainable<Element>
+//     }
+//   }
+// }
+
+Cypress.Commands.add('login', () => {
+  cy.visit('/login');
+  cy.get('[data-cy="auth-email"]').click();
+  cy.get('[data-cy="auth-email"]').type('test@example.com');
+  cy.get('[data-cy="auth-password"]').type('testpassword');
+  cy.get('[data-cy="auth-submit"]').click();
+  cy.location('pathname').should('eq', '/takeaways');
+  cy.getCookie('__session').its('value').should('not.be.empty');
+});
+
+// the below code snippet is required to handle a React hydration bug that would cause tests to fail
+// it's only a workaround until this React behavior / bug is fixed
+Cypress.on('uncaught:exception', (err) => {
+  // we check if the error is
+  if (
+    err.message.includes('Minified React error #418;') ||
+    err.message.includes('Minified React error #423;') ||
+    err.message.includes('hydrating') ||
+    err.message.includes('Hydration')
+  ) {
+    return false;
+  }
+});
